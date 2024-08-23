f281bf335fa4991690657262dc453202ad153c8d
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-08-23 16:45:22 -0300

    :sparkles: feat: Adicionando componente header

---
+++ src/components/header.tsx
@@ -0,0 +1,38 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+import { ToggleTheme } from "./toggle-theme";
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-10 backdrop-blur-sm bg-background/80 border-b">
+      <div className=" px-4 py-4 flex justify-end items-end">
+        <div className="flex items-center gap-4">
+          <div className=" flex items-center gap-3">
+            <a
+              href="https://github.com/santozxy"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-foreground hover:text-primary"
+            >
+              <Github size={20} />
+            </a>
+            <a
+              href="https://linkedin.com/in/monnuery-junior"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-foreground hover:text-primary"
+            >
+              <Linkedin size={20} />
+            </a>
+            <a
+              href="mailto:monnuerypachecodossantos@gmail.com"
+              className="text-foreground hover:text-primary"
+            >
+              <Mail size={20} />
+            </a>
+          </div>
+          <ToggleTheme />
+        </div>
+      </div>
+    </header>
+  );
+}
