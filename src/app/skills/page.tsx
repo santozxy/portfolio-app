4c27bc5d332dfdaaf5ac3f45c7c6d46a861f2449
Monnuery Júnior <118072648+santozxy@users.noreply.github.com>
2024-08-24 01:31:13 -0300

    Update page.tsx

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -2,7 +2,7 @@ import { Progress } from "@/components/ui/progress";
 import React from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "../../components/ui/card";
 
-export function Skills() {
+export default function Skills() {
   const skills = [
     {
       title: "React Native",
