12e0d51d5c89cda3e41a0a21e4322f11780bb602
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-08-28 15:22:33 -0300

    :sparkles: feat: Add component locale-switcher

---
+++ src/components/locale-switcher.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useLocale, useTranslations } from "next-intl";
+import { useRouter, usePathname } from "next/navigation";
+import { useTransition } from "react";
+import { Globe } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export function LocaleSwitcher() {
+  const t = useTranslations("LocaleSwitcher");
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const pathname = usePathname();
+  const locale = useLocale();
+
+  const onLocaleChange = (nextLocale: string) => {
+    startTransition(() => {
+      const segments = pathname.split('/').filter(Boolean);
+      segments[0] = nextLocale;
+      const newPathname = `/${segments.join('/')}`;
+      router.push(newPathname);
+    });
+  };
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Globe className="h-6 w-6  text-muted-foreground" aria-hidden="true" />
+      <Select
+        defaultValue={locale}
+        onValueChange={onLocaleChange}
+        disabled={isPending}
+      >
+        <SelectTrigger className="sm:w-[130px]" aria-label={t("changeLanguage")}>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="en">{t("english")}</SelectItem>
+          <SelectItem value="pt">{t("portuguese")}</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
