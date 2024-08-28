d0e4a0e1a7b777cac62b42c8f68f979e925b85e2
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-08-28 00:02:15 -0300

    :bug: fix: Resolver imports

--- src/app/(portfolio)/skills/page.tsx
+++ src/app/(portfolio)/skills/page.tsx
@@ -1,6 +1,6 @@
-import { Progress } from "@/components/ui/progress";
+
 import React from "react";
-import { Card, CardHeader, CardTitle, CardContent } from "../../components/ui/card";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 export default function Skills() {
   const skills = [
