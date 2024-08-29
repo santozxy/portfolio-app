319a1dc3cbaec941c5c0fbac9be13ea9f2f8e837
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-08-28 23:48:24 -0300

    :lipstick: feat: improvements UI UX

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -37,7 +37,7 @@ export function LocaleSwitcher() {
         disabled={isPending}
       >
         <SelectTrigger
-          className="bg-opacity-10 bg-gradient-to-tl from-black via-zinc-700/30 to-zinc-900 "
+          className="sm:w-[130px] w-24 bg-opacity-10 bg-gradient-to-tl from-black via-zinc-700/30 to-zinc-900 "
           aria-label={t("changeLanguage")}
         >
           <SelectValue />
